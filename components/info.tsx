b1d541ef288702fd8d796f327a3602c2271ca6a1
Naman Pandey <naman.0816@gmail.com>
2024-10-16 15:48:17 +0530

    fixed some issues

--- components/info.tsx
+++ components/info.tsx
@@ -3,8 +3,13 @@ import { Product } from "@/types";
 import Currency from "./ui/currency";
 import Button from "./ui/button";
 import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 const Info = ({ data }: { data: Product }) => {
+  const cart = useCart();
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -28,7 +33,7 @@ const Info = ({ data }: { data: Product }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart />
         </Button>
